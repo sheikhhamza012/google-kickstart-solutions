2bedeb71d8dd22aca968c94fb3d4e4e0c37b682e
sheikh hamza <sheikhhamza012@gmail.com>
2020-08-30 22:27:13 +0500

    Initial Commit

---
+++ google-kickstart/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  google-kickstart
+//
+//  Created by sheikh hamza on 30/08/2020.
+//  Copyright © 2020 sheikh hamza. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
