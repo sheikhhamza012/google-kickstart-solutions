cf6e9b8584c92b1782a003cc791a63518e3712ad
sheikh hamza <sheikhhamza012@gmail.com>
2020-09-01 20:52:52 +0500

    allocation problem from 2020-A

---
+++ google-kickstart/2020/A/allocation.cpp
@@ -0,0 +1,9 @@
+//
+//  allocation.cpp
+//  google-kickstart
+//
+//  Created by sheikh hamza on 30/08/2020.
+//  Copyright © 2020 sheikh hamza. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ google-kickstart/headers.h
@@ -0,0 +1,13 @@
+//
+//  headers.h
+//  google-kickstart
+//
+//  Created by sheikh hamza on 30/08/2020.
+//  Copyright © 2020 sheikh hamza. All rights reserved.
+//
+
+#ifndef headers_h
+#define headers_h
+
+
+#endif /* headers_h */
